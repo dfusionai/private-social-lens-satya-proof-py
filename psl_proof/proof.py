e4fe34ce60778e2f0db2dafcc1f988e71d3ba171
Vaughn <vaughn@mysoft.co.za>
2025-12-23 00:14:47 +0200

    comment

--- psl_proof/proof.py
+++ psl_proof/proof.py
@@ -51,7 +51,7 @@ class Proof:
         )
         is_data_authentic = verify_result
         cooldown_period_hours = 4  # Safe default
-        minimum_score = 0.0001  # Safe default
+        minimum_score = 0.0001  # Safe default. 0.01 / 100
         if is_data_authentic:
             #print(f"verify_result: {verify_result}")
             is_data_authentic = verify_result.is_valid
